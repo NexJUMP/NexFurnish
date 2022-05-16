f16ff455098a30c12e7f0bad0edeb49b8762a102
Seremet Mihail <misha.seremet@mail.ru>
2022-05-16 11:50:07 +0300

    main

--- categorie-1/js/index.js
+++ categorie-1/js/index.js
@@ -1,7 +1,21 @@
+const moneys = document.querySelectorAll('.categorie__wrapper-money');
+let arr = []
+
+moneys.forEach((money)=>{
+    const text = money.textContent.split('lei');
+    const numberMoney = Number(text[0]);
+    money.setAttribute('data-price', numberMoney);
+    arr.push(money)
+})
+
+
+
 const buttonsSort = document.querySelectorAll('.categorie__sort-by div');
 
-buttonsSort.forEach((button) =>{
-    button.addEventListener('click', ()=>{
+
+buttonsSort.forEach((button) => {
+    button.addEventListener('click', () => {
         console.log('click');
     });
 });
+
